e62133c10df67c016a8d1e3deb8be05f0e970ba4
hắc cơ lỏ <phanhuynhphuckhang12c8@gmail.com>
2025-04-08 22:10:05 +0700

    Initial commit (exclude demoCICD)

---
+++ src/main/java/fpoly/Calculator.java
@@ -0,0 +1,20 @@
+package fpoly;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Không thể chia cho 0");
+        return a / b;
+    }
+}
